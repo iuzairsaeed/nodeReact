15e22a27a98d6681df8c01ec56bbfd9003e6ef3a
Uzair Saeed <uzair.saeed27@gmail.com>
2020-04-12 09:22:13 +0500

    add key in Google Stretegy

--- services/passport.js
+++ services/passport.js
@@ -20,6 +20,7 @@ passport.use(
             clientSecret: keys.googleClientSecret,
             clientID: keys.googleClientID,
             callbackURL: "/auth/google/callback",
+            proxy: true,
         },
         (accessToken, refreshToken, profile, done) => {
             User.findOne({ googleId: profile.id }).then((existingUser) => {
